bece8c8305f48ef3e9540fb39a0fb33ab769627f
OscarM24 <61362837+Oscaronga12211@users.noreply.github.com>
2020-09-15 13:34:34 -0500

    pruebas

--- Proyectos_Java/Prueba.java
+++ Proyectos_Java/Prueba.java
@@ -1,6 +1,6 @@
 package     Proyectos_Java;
 class prueba{
     public static void main(String[] args) {
-        System.out.println("Holis");
+        System.out.println("Holis como estas ?");
     }
 }
