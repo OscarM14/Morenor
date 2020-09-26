680356d0bcecd1c6a0ac1f9d40c13f6d0eda37b3
OscarM24 <61362837+Oscaronga12211@users.noreply.github.com>
2020-09-26 16:30:45 -0500

    listo

---
+++ Proyectos/src/datos/Fibo.java
@@ -0,0 +1,90 @@
+package datos;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Fibo extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtNumero;
+	private JTextField txtResultado;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		Fibo f = new Fibo();
+		f.setVisible(true);
+		
+		
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Fibo() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("FIBONACCI");
+		lblNewLabel.setBounds(167, 0, 79, 30);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Numero de Consulta");
+		lblNewLabel_1.setBounds(10, 42, 115, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Resultado");
+		lblNewLabel_2.setBounds(10, 67, 96, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		txtNumero = new JTextField();
+		txtNumero.setBounds(150, 41, 96, 20);
+		contentPane.add(txtNumero);
+		txtNumero.setColumns(10);
+		
+		txtResultado = new JTextField();
+		txtResultado.setEditable(false);
+		txtResultado.setBounds(150, 64, 96, 20);
+		contentPane.add(txtResultado);
+		txtResultado.setColumns(10);
+		
+		JButton btnConsultar = new JButton("Consultar");
+		btnConsultar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				capturar();
+			}
+			void capturar() {
+				int n;
+				n=Integer.parseInt(txtNumero.getText());
+				int a=0,b=1,c=0,e=1;
+				for(int i =2;i<=n;i++) {
+					c=a+b;
+					e+=c;
+					a=b;
+					b=c;
+				}
+				txtResultado.setText(Integer.toString(c));
+				
+			}
+			void Borrar() {
+				
+			}
+		});
+		btnConsultar.setBounds(117, 152, 89, 23);
+		contentPane.add(btnConsultar);
+	}
+}
